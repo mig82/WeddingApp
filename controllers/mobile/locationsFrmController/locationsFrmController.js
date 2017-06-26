339a2fd7287f4f27bb59bc638a6ebc26b9ffad66
mig82 <miguelangelxfm@gmail.com>
2017-06-26 18:29:46 +0200

    Add external navigation to church, hotel and map

--- controllers/mobile/locationsFrmController/locationsFrmController.js
+++ controllers/mobile/locationsFrmController/locationsFrmController.js
@@ -17,7 +17,18 @@ define({
             	router.go2('home', {});
             };
         });
+      
+      	$frm.goToHotelFlx.onTouchEnd = function _goToHotelFlx_onTouchEnd (){
+          	kony.application.openURL('http://www.ayrehoteles.com/hoteles/ayre-gran-hotel-colon/');
+        };
       	
+      	$frm.goToTempleFlx.onTouchEnd = function _goToTempleFlx_onTouchEnd (){
+          	kony.application.openURL('http://www.bsmiguel.es/');
+        };
+      
+      	$frm.getDirectionsBtn.onTouchEnd = function _getDirectionsBtn_onTouchEnd(){
+          	kony.application.openURL('https://www.google.es/maps/dir/Basilica+Pontificia+de+san+Miguel,+Calle+de+San+Justo,+4,+28005+Madrid/Ayre+Gran+Hotel+Col%C3%B3n,+Calle+del+Pez+Volador,+Madrid/@40.4141431,-3.7055437,14z/am=t/data=!4m14!4m13!1m5!1m1!1s0xd422878ec114737:0x7a2dc3e0a1cbf76a!2m2!1d-3.7097013!2d40.414254!1m5!1m1!1s0xd4226026f361355:0x8acfecb6850cf07b!2m2!1d-3.669204!2d40.413003!5i1');
+        };
     },
     
   	_onHide: function _onHide() {},
