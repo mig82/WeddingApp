157a226c2ee92cf75657bdfedf069d816d2607ed
mig82 <miguelangelxfm@gmail.com>
2018-04-14 21:39:44 +0200

    Set status bar color

---
+++ studioactions/mobile/appEvents.js
@@ -0,0 +1,8 @@
+define({
+    AS_AppEvents_d305706a3453452db097243e240ec5e8: function AS_AppEvents_d305706a3453452db097243e240ec5e8(eventobject) {
+        var self = this;
+        kony.application.setApplicationProperties({
+            statusBarColor: "b33661"
+        });
+    }
+});
