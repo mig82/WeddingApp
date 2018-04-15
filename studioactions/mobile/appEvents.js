a52fa44008e741db20a3afb36c40c98290d6ad03
mig82 <miguelangelxfm@gmail.com>
2018-04-15 11:50:25 +0200

    Fix postapp init to set status bar color

--- studioactions/mobile/appEvents.js
+++ studioactions/mobile/appEvents.js
@@ -1,6 +1,7 @@
 define({
-    AS_AppEvents_d305706a3453452db097243e240ec5e8: function AS_AppEvents_d305706a3453452db097243e240ec5e8(eventobject) {
+    AS_AppEvents_eecb8c80398b4c85ad212ebde17edbee: function AS_AppEvents_eecb8c80398b4c85ad212ebde17edbee(eventobject) {
         var self = this;
+        //Change the background color of the Android status bar.
         kony.application.setApplicationProperties({
             statusBarColor: "b33661"
         });
